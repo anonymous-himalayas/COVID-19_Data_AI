54a6e7f5b98e9d484fef9d20208f6afa5aed23d7
Himal Malik <himalm100@gmail.com>
2024-08-18 12:43:47 -0700

    added main function and cleaned everything

--- main.py
+++ main.py
@@ -7,32 +7,31 @@ from note_engine import note_engine
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai import OpenAI 
-from read_pdf import covid_engine
-
+from read_pdf import covid_pdf_engine
 load_dotenv()
 
-covid_data = os.path.join("datasets", "country_wise_latest.csv")
-covid_df = pd.read_csv(covid_data)
+def main():
+    covid_data = os.path.join("datasets", "country_wise_latest.csv")
+    covid_df = pd.read_csv(covid_data)
+    covid_query_engine = PandasQueryEngine(df=covid_df, instruction_str=instruction_str)
+    covid_query_engine.update_prompts({'pandas_prompt': new_prompt})
+    tools = [
+        note_engine,
+        QueryEngineTool(query_engine=covid_query_engine, metadata=ToolMetadata(
+            name = 'Covid data',
+            description = 'This gives information about the COVID-19 virus in various countries such as cases and death counts.'
+        )),
+        QueryEngineTool(query_engine=covid_pdf_engine, metadata=ToolMetadata(
+            name = 'COVID pdf data',
+            description = 'This gives information about the COVID-19 virus at its base level including its structure and how it spreads. In addition, it describes the symptoms and how to prevent the spread of COVID-19.'
+        ))
+    ]
+    llm = OpenAI(model='gpt-3.5-turbo-0613')
+    agent = ReActAgent(tools=tools, llm=llm, verbose=True, context=context)
 
-#print(covid_df.head())
+    while (prompt := input('Enter a prompt (q to quit): ')) != 'q':
+        result = agent.query(prompt)
+        print(result)
 
-
-covid_query_engine = PandasQueryEngine(df=covid_df, instruction_str=instruction_str)
-
-covid_query_engine.update_prompts({'pandas_prompt': new_prompt})
-
-
-tools = [
-    note_engine,
-    QueryEngineTool(query_engine=covid_query_engine, metadata=ToolMetadata(
-        name = 'Covid data',
-        description = 'This gives information about the COVID-19 virus in various countries such as cases and death counts.',
-    ))
-]
-
-llm = OpenAI(model='gpt-3.5-turbo-0613')
-agent = ReActAgent(tools=tools, llm=llm, verbose=True, context=context)
-
-while (prompt := input('Enter a prompt (q to quit): ')) != 'q':
-    result = agent.query(prompt)
-    print(result)
+if __name__ == '__main__':
+    main()
