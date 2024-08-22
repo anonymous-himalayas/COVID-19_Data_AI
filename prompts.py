5070bc5cc772ae6ff4717826ba2e7d131340856f
Himal <hpmalik@uci.edu>
2024-08-22 12:28:11 -0700

    redid prompts

--- prompts.py
+++ prompts.py
@@ -1,30 +1,27 @@
 from llama_index.core import PromptTemplate
 
+
 instruction_str = """
-    1. Convert the query to executable Python code using the Pandas Library.
-    2. The final line of code should be a python expression that can be called with an eval() function.
-    3. The Pandas DataFrame is already loaded as 'df'.
-    4. The Pandas Library is already imported as 'pd'.
-    5. The code should be a solution to the query.
-    6. Print only the expression.
-    7. Do not quote the expression.
-"""
+    1. Convert the query to executable Python code using Pandas.
+    2. The final line of code should be a Python expression that can be called with the `eval()` function.
+    3. The code should represent a solution to the query.
+    4. PRINT ONLY THE EXPRESSION.
+    5. Do not quote the expression."""
+
 
 new_prompt = PromptTemplate(
     """
-    You are working with a pandas dataframe in Python about COVID-19 data.
-    The name of this dataframe is covid_df.
-    This is the result of `print(covid_df.head())`:
+    You are working with a pandas dataframe in Python.
+    The name of the dataframe is `df`.
+    This is the result of `print(df.head())`:
     {df_str}
 
-    Follow these instructions:
+    Follow these instructions using the tools provided:
     {instruction_str}
-    This is the query that needs to be executed:
     Query: {query_str}
 
-    Expression 
+    Expression: """
+)
 
-""")
-context = """Purpose: The primary role of this tool is to provide a users a detailed overview of the COVID-19 virus in various countries. 
-This includes information such as cases and death counts and is useful for tracking the spread of the virus. In addition, this tool allows users to query the database and 
-retrieve specific information. This tool is designed to be user-friendly and easy to use, making it accessible to a wide range of users."""
+context = """Purpose: The primary role of this agent is to assist users by providing accurate 
+            information about COVID-19 and how it has impacted the world and describe information and how COVID-19 works USING the tools of the pdf and the dataset. """
